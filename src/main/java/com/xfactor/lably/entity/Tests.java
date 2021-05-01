621b0385ce1c084ace08b1bc16f64e9f23c54d46
Prachi Sharma <sharmabandana2000@gmail.com>
2021-05-01 16:04:15 +0530

    entities created

---
+++ src/main/java/com/xfactor/lably/entity/Tests.java
@@ -0,0 +1,36 @@
+package com.xfactor.lably.entity;
+
+//import org.springframework.context.annotation.Description;
+
+public class Tests {
+    
+    private String name;
+
+    private String description;
+
+    private double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
